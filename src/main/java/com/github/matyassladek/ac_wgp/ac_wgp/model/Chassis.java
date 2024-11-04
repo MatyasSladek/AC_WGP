66f349d4a9e379bc2c17d618d82fbeb82c22755b
Matyáš Sládek <matyas.sladek@email.cz>
2024-11-04 21:59:51 +0100

    Chassis and Engine counstructor edited, so it is correctly now.

--- src/main/java/com/github/matyassladek/ac_wgp/ac_wgp/model/Chassis.java
+++ src/main/java/com/github/matyassladek/ac_wgp/ac_wgp/model/Chassis.java
@@ -1,4 +1,8 @@
 package com.github.matyassladek.ac_wgp.ac_wgp.model;
 
 public class Chassis extends VehiclePart {
+    public Chassis() {
+        super(200, new int[]{20, 25, 25, 20, 20, 15, 15, 10, 10, 10, 10, 5, 5, 5, 5},
+                5, 2_000_000, 50_000);
+    }
 }
--- src/main/java/com/github/matyassladek/ac_wgp/ac_wgp/model/Engine.java
+++ src/main/java/com/github/matyassladek/ac_wgp/ac_wgp/model/Engine.java
@@ -2,7 +2,8 @@ package com.github.matyassladek.ac_wgp.ac_wgp.model;
 
 public class Engine extends VehiclePart {
 
-    public Engine(int initialPerformance, int[] upgradeSpeed, int upgradeValue, int basicUpgradeCost, int upgradeCostIncrease) {
-        super(initialPerformance, upgradeSpeed, upgradeValue, basicUpgradeCost, upgradeCostIncrease);
+    public Engine() {
+        super(100, new int[]{7, 8, 9, 10, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+                1, 1_000_000, 10_000);
     }
 }
