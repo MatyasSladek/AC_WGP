f3d39f56bd3f5a82f86f5f9dd8afd281fcdaa6ec
Matyáš Sládek <matyas.sladek@email.cz>
2024-11-04 22:07:29 +0100

    VehiclePart constructor made protected. Method upgrade added.

--- src/main/java/com/github/matyassladek/ac_wgp/ac_wgp/model/VehiclePart.java
+++ src/main/java/com/github/matyassladek/ac_wgp/ac_wgp/model/VehiclePart.java
@@ -7,12 +7,21 @@ public abstract class VehiclePart {
     protected final int upgradeValue;
     protected final int basicUpgradeCost;
     protected final int upgradeCostIncrease;
+    protected int performance;
+    protected int upgradeCost;
 
-    public VehiclePart(int initialPerformance, int[] upgradeSpeed, int upgradeValue, int basicUpgradeCost, int upgradeCostIncrease) {
+    protected VehiclePart(int initialPerformance, int[] upgradeSpeed, int upgradeValue, int basicUpgradeCost, int upgradeCostIncrease) {
         this.initialPerformance = initialPerformance;
         this.upgradeSpeed = upgradeSpeed;
         this.upgradeValue = upgradeValue;
         this.basicUpgradeCost = basicUpgradeCost;
         this.upgradeCostIncrease = upgradeCostIncrease;
+        this.performance = initialPerformance;
+        this.upgradeCost = basicUpgradeCost;
+    }
+
+    public void upgrade() {
+        this.performance -= this.upgradeValue;
+        this.upgradeCost += this.upgradeCostIncrease;
     }
 }
