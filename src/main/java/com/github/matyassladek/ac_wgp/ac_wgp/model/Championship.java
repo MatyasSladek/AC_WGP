ea9998a9c52143ef849c1b1c3b867f3a46acb81b
Matyáš Sládek <matyas.sladek@email.cz>
2024-11-05 13:59:05 +0100

    Drriver record created, Championship class created.

---
+++ src/main/java/com/github/matyassladek/ac_wgp/ac_wgp/model/Championship.java
@@ -0,0 +1,24 @@
+package com.github.matyassladek.ac_wgp.ac_wgp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Setter
+public class Championship {
+
+    private static int season = 0;
+    private final Tracks[] calendar;
+    private final int[] scoring;
+    private int currentRound;
+    private List<HashMap<Driver, Integer>> standings;
+
+    public Championship(Tracks[] calendar) {
+        this.calendar = calendar;
+        this.scoring = new int[]{50, 30, 20, 15, 10, 5, 4, 3, 2, 1};
+        this.currentRound = 0;
+    }
+}
---
+++ src/main/java/com/github/matyassladek/ac_wgp/ac_wgp/model/Driver.java
@@ -0,0 +1,4 @@
+package com.github.matyassladek.ac_wgp.ac_wgp.model;
+
+public record Driver(String firstName, String lastName, Country country) {
+}
