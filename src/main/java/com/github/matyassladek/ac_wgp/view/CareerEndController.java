7058307ab41b1f2bb465d667f5911c9ef127ca1f
Matyáš Sládek <matyas.sladek@email.cz>
2024-12-20 11:50:48 +0100

    Code cleaned.

--- src/main/java/com/github/matyassladek/ac_wgp/view/CareerEndController.java
+++ src/main/java/com/github/matyassladek/ac_wgp/view/CareerEndController.java
@@ -1,12 +1,6 @@
 package com.github.matyassladek.ac_wgp.view;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class CareerEndController extends ViewController {
-
-    @FXML
-    private Label text;
 
     public CareerEndController() {
         super(FXMLFile.CAREER_END.getFileName());
