155b9268b244bb7cb794484d4b4d0e6ff4c6ac68
Matyáš Sládek <matyas.sladek@email.cz>
2025-01-26 11:32:57 +0100

    Season calendars update.

--- src/main/java/com/github/matyassladek/ac_wgp/enums/Track.java
+++ src/main/java/com/github/matyassladek/ac_wgp/enums/Track.java
@@ -12,16 +12,11 @@ public enum Track {
     BARCELONA("Circuit de Barcelona-Catalunya", Country.ESP, calculateLaps(4675)),
     BRNO("Brno Circuit", Country.CZE, calculateLaps(5403)),
     CHANG("Chang International Circuit", Country.THA, calculateLaps(4554)),
-    IMOLA("Autodromo Internazionale Enzo e Dino Ferrari", Country.ITA, calculateLaps(4909)),
-    INDIANAPOLIS("Indianapolis Grand Prix Course", Country.USA, calculateLaps(4023)),
     INTERLAGOS("Autódromo José Carlos Pace", Country.BRA, calculateLaps(4309)),
     KYALAMI("Kyalami Grand Prix Circuit", Country.ZAF, calculateLaps(4529)),
-    LE_MANS_BUGATTI("Circuit Bugatti", Country.FRA, calculateLaps(4185)),
     MELBOURNE("Albert Park Circuit", Country.AUS, calculateLaps(5303)),
-    MONTE_CARLO("Circuit de Monaco", Country.MCO, calculateLaps(3337)),
     MONTREAL("Circuit Gilles Villeneuve", Country.CAN, calculateLaps(4361)),
     MONZA("Autodromo Nazionale Monza", Country.ITA, calculateLaps(5793)),
-    MOST("Autodrom Most", Country.CZE, calculateLaps(4219)),
     MUGELLO("Mugello Circuit", Country.ITA, calculateLaps(5245)),
     NURBURGRING("Nürburgring", Country.DEU, calculateLaps(5148)),
     ROAD_AMERICA("Road America", Country.USA, calculateLaps(6515)),
@@ -29,9 +24,9 @@ public enum Track {
     SILVERSTONE("Silverstone Circuit", Country.GBR, calculateLaps(5891)),
     SPA_FRANCORCHAMPS("Circuit de Spa-Francorchamps", Country.BEL, calculateLaps(7004)),
     SEPANG("Sepang International Circuit", Country.MYS, calculateLaps(5543)),
+    SPIELBERG("Spielberg Circuit", Country.AUT, calculateLaps(	4326)),
     SUZUKA("Suzuka International Racing Course", Country.JPN, calculateLaps(5807)),
-    WATKINS_GLEN("Watkins Glen International", Country.USA, calculateLaps(5472)),
-    ZANDVOORT("Circuit Zandvoort", Country.NLD, calculateLaps(4259));
+    WATKINS_GLEN("Watkins Glen International", Country.USA, calculateLaps(5472));
 
     private final String name;
     private final Country country;
