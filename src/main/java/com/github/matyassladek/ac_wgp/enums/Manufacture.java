2410d3b9f6694bff6680385e290aaa160c4ec309
Matyáš Sládek <matyas.sladek@email.cz>
2025-10-21 20:11:04 +0200

    Car skin names fixed in Manufacture enum.

--- src/main/java/com/github/matyassladek/ac_wgp/enums/Manufacture.java
+++ src/main/java/com/github/matyassladek/ac_wgp/enums/Manufacture.java
@@ -7,16 +7,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Manufacture {
 
-    BWT("BWT Racing", "BWT", Country.AUT, "ferrari_f2002", "30_bwt", "31_bwt"),
+    BWT("BWT Racing", "BWT", Country.AUT, "ferrari_f2002", "42_bwt", "43_bwt"),
     CASTROL("Castrol Racing", "Castrol", Country.GBR, "ferrari_f2002", "36_castrol", "37_castrol"),
     EM("Edl Motorsport", "EM", Country.CZE, "ferrari_f2002", "38_em", "39_em"),
     GULF("Gulf Oil Motorsports", "Gulf", Country.USA, "ferrari_f2002", "34_gulf", "35_gulf"),
     JPS("Team John Player Special", "JPS", Country.GBR, "ferrari_f2002", "26_jps", "27_jps"),
-    MARTINI("Martini Racing", "Martini", Country.ITA, "ferrari_f2002", "40_martini", "41_martini"),
+    MARTINI("Martini Racing", "Martini", Country.ITA, "ferrari_f2002", "30_martini", "31_martini"),
     NEWMAN("Newman Motorsport", "Newman", Country.USA, "ferrari_f2002", "28_newman", "29_newman"),
     RBR("Oracle Red Bull Racing", "Red Bull", Country.AUT, "ferrari_f2002", "32_rbr", "33_rbr"),
     TARGET("Target Racing", "Target", Country.USA, "ferrari_f2002", "46_target", "47_target"),
-    VODAFONE("Vodafone Racing", "Vodafone", Country.GBR, "ferrari_f2002", "42_vodafone", "43_vodafone");
+    VODAFONE("Vodafone Racing", "Vodafone", Country.GBR, "ferrari_f2002", "44_vodafone", "45_vodafone");
 
     private final String nameLong;
     private final String nameShort;
